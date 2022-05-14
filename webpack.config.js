d237b1b177ba899e6979819100b9965f4c5e24fd
initial@gmail.com <teamdailypractice@gmail.com>
2022-05-14 17:46:16 +0800

    customize output filename and directory

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
-const path = require("path")
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
     mode: "development",
     entry: ['./src/entry', './src/index'],
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: "output.js"
+        filename: "webpack-output.js"
     }
 }
