474721c398bffc55bbd0b4260428ee0179370084
initial@gmail.com <teamdailypractice@gmail.com>
2022-05-15 01:34:10 +0800

    fixing html header in config

--- webpack.config.js
+++ webpack.config.js
@@ -23,9 +23,11 @@ module.exports = {
                     htmlAttributes: {
                         lang: 'en'
                     },
-                    head: [`<link rel="stylesheet" href="styles/styles.css">
-                        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                        <meta name="viewport" content="width=device-width, initial-scale=1.0">`]
+                    head: [`
+      <link rel="stylesheet" href="styles/styles.css">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">`
+                    ]
                 },
 
             }
