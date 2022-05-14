5ee826e3243091076773a651db787abed08f6a20
initial@gmail.com <teamdailypractice@gmail.com>
2022-05-14 17:37:31 +0800

    webpack multiple entry files

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # webpack-learning
 
+* Tutorial <https://www.smashingmagazine.com/2021/06/getting-started-webpack/>
+
 ## dev steps
 
 * npm init -y
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path")
 
 module.exports = {
     mode: "development",
-    entry: "./src/entry",
+    entry: ['./src/entry', './src/index'],
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "output.js"
